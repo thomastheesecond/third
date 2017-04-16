ae62eb3445c7e810de530cd6818a4893e6d73487
thomastheesecond <tmackey@adrian.edu>
2017-04-16 14:47:28 -0400

    Guessing Game
    Guessing Game

---
+++ guess.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Tom Mackey's Guessing Game</title>
+</head>
+<body>
+<h1>Welcome to my guessing game</h1>
+<p>
+<?php
+  if ( ! isset ($_GET['guess'])){
+    echo ("No guess");
+  } else if (strlen($_GET['guess'])<1){
+    echo ("Too short");
+  } else if (! is_numeric($_GET['guess'])){
+    echo ("No number");
+  } else if ($_GET['guess'] <74){
+    echo ("Too low");
+  } else if ($_GET)['guess'] >74) {
+  echo ("Too high");
+} else {
+  echo("Correct");
+}
+?>
+</p>
+</body>
+</html>
